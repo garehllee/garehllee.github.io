740a227ca4eea9b0686a2ac24dc1404167c6683a
garehllee <garehllee@gmail.com>
2023-12-26 05:39:54 -0500

    changes

---
+++ jv.js
@@ -0,0 +1,28 @@
+// window.addEventListener('scroll', () => {
+//     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+//   }, false);
+
+
+
+function checkScroll(){
+  let compscroll = .45;
+  let wholepage = document.getElementById('wholepage');
+  let colorreg = getComputedStyle(document.body).getPropertyValue('--reg');
+    if ((window.pageYOffset / (document.body.offsetHeight - window.innerHeight))>compscroll && colorreg=="black"){
+      if (wholepage.classList.contains('wholepagelight')){
+        wholepage.classList.remove('wholepagelight');
+      }
+      wholepage.classList.add('wholepagedark');
+    }
+    else{
+      if ((window.pageYOffset / (document.body.offsetHeight - window.innerHeight))<compscroll && colorreg=="white"){
+        if(wholepage.classList.contains('wholepagedark')){
+          wholepage.classList.remove('wholepagedark');
+        }
+        wholepage.classList.add('wholepagelight');
+      }
+    }
+  };
+
+window.addEventListener('scroll', checkScroll);
+
