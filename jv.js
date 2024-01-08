e427f4380d65bf85919d2c9910401c47cbe17d66
garehllee <garehllee@gmail.com>
2024-01-08 01:21:59 -0500

    adding stuff to homepage

--- jv.js
+++ jv.js
@@ -1,6 +1,6 @@
-// window.addEventListener('scroll', () => {
-//     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-//   }, false);
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
 
 
 
